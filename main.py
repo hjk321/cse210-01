401f5308bce1937a3a0026c2ff33dfcfbcc67e85
hjk321 <37224753+hjk321@users.noreply.github.com>
2022-01-13 21:18:38 -0700

    Check Win/Draw

--- main.py
+++ main.py
@@ -66,7 +66,36 @@ def turn_input(boards, player):
         print("Space already taken.")
 
 def check_game_end(boards):
-    return NONE
+    def check_spaces(board, a, b, c):
+        if board[a] == board[b] == board[c]:
+            return board[a]
+        return NONE
+
+    # We'll check each board independently since they all have the same rules.
+    # If a win is found on a board, the function early-exits out.
+    for board in boards:
+        val = check_spaces(board, 0, 1, 2)
+        if val != NONE: return val
+        val = check_spaces(board, 3, 4, 5)
+        if val != NONE: return val
+        val = check_spaces(board, 6, 7, 8)
+        if val != NONE: return val
+        val = check_spaces(board, 0, 3, 6)
+        if val != NONE: return val
+        val = check_spaces(board, 1, 4, 7)
+        if val != NONE: return val
+        val = check_spaces(board, 2, 5, 8)
+        if val != NONE: return val
+        val = check_spaces(board, 0, 4, 8)
+        if val != NONE: return val
+        val = check_spaces(board, 6, 4, 2)
+        if val != NONE: return val
+
+    # No win found. We now need to check for empty spaces, and declare a draw if not.
+    for board in boards:
+        for space in board:
+            if space == NONE: return NONE
+    return DRAW
 
 if __name__ == "__main__":
     main()
